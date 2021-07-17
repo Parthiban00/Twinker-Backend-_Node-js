a4dbe43e77174fd76e1460ceecee4701e8917ffb
Parthiban <85576418+Parthiban00@users.noreply.github.com>
2021-07-17 12:07:38 +0530

    db error fixed

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect( 'mongodb://127.0.0.1:27017/twinkertest', {useNewUrlParser :true, useUnifiedTopology: true, useFindAndModify: false})
+//mongoose.connect( 'mongodb://127.0.0.1:27017/twinkertest', {useNewUrlParser :true, useUnifiedTopology: true, useFindAndModify: false})
+mongoose.connect( 'mongodb://parthiban:parthi123@207.180.242.26:27017/twinkertest?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false', {useNewUrlParser :true, useUnifiedTopology: true, useFindAndModify: false})
 //mongoose.connect('mongodb+srv://Twinker-admin:Parthi12345@cluster0.y4zil.mongodb.net/twinkerprod', {useNewUrlParser :true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connnected"))
     .catch((error) => console.log(error));
